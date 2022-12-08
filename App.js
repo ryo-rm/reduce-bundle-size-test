e8d9fddd87488eff6ea2637fcd3f83e93f9dabf1
ryo-rm <6457344+ryo-rm@users.noreply.github.com>
2022-12-08 13:29:41 +0900

    chore: newrelic-agent-v8

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import NewRelic from 'newrelic-react-native-agent';
+
 export default function App() {
   return (
     <View style={styles.container}>
